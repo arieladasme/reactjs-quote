170c169fc39cd724ac45e8d78b855ba2a7079c79
Ariel <ariel.wm1@gmail.com>
2020-07-08 15:29:03 -0400

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import styled from '@emotion/styled'
 import Header from './components/Header'
 
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const FormContainer = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`
+
 function App() {
-  return <Header title="Cotizador de Seguros" />
+  return (
+    <Container>
+      <Header title="Cotizador de Seguros" />
+      <FormContainer></FormContainer>
+    </Container>
+  )
 }
 
 export default App
